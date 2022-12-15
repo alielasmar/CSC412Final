d0fece2075e3e93d183fbaf90cb1e73303b9c2e9
DylanChelo <dylan_chelo@uri.edu>
2022-12-15 14:53:13 -0500

    Created and started the thread function and a fucntion to check the next square

--- Final Project - Handout/Code-Handout/main.cpp
+++ Final Project - Handout/Code-Handout/main.cpp
@@ -32,6 +32,8 @@ Direction newDirection(Direction forbiddenDir = Direction::NUM_DIRECTIONS);
 TravelerSegment newTravelerSegment(const TravelerSegment& currentSeg, bool& canAdd);
 void generateWalls(void);
 void generatePartitions(void);
+void threadFunc(struct TravelerToPass localTraveler);
+bool checkNextSquare(struct TravelerToPass localTraveler, Direction currentDir);
 void pathFinding(struct TravelerToPass localTraveler);
 
 //==================================================================================
@@ -365,6 +367,99 @@ void initializeApplication(void)
 //   Else if (Wall) {pathFinding() }
 //
 //}
+void threadFunc(struct TravelerToPass localTraveler){
+	bool goalReached = false;
+	bool movingToGoal, colMove, rowMove, isNextFree;
+	int currentRow, currentCol;
+	Direction currentDir, colDir, rowDir;
+
+	while(goalReached != true){
+		currentRow = localTraveler.travelersPassed[0][localTraveler.travelerIdx].segmentList[0].row;
+		currentCol = localTraveler.travelersPassed[0][localTraveler.travelerIdx].segmentList[0].col;
+		currentDir = localTraveler.travelersPassed[0][localTraveler.travelerIdx].segmentList[0].dir;
+		
+		//Check north/south movement
+		if(currentRow > exitPos.row){
+			rowDir = Direction::NORTH;
+			rowMove = true;
+		}
+		else if (currentRow > exitPos.row){
+			rowDir = Direction::SOUTH;
+			rowMove = true;
+		}
+		else{
+			rowMove = false;
+			rowDir = Direction::WEST;
+		}
+		//Check east/west movement
+		if(currentCol > exitPos.col){
+			colDir = Direction::WEST;
+			colMove = true;
+		}
+		else if (currentRow > exitPos.row){
+			colDir = Direction::EAST;
+			colMove = true;
+		}
+		else{
+			colMove = false;
+			colDir = Direction::NORTH;
+		}
+		
+		//Check to see if Traveler is moving towards the goal
+		if(currentDir == colDir && colMove == true){
+			//Check for wall then move
+			isNextFree = checkNextSquare(localTraveler, currentDir);
+			moveTraveler(localTraveler);
+		}
+		else if(currentDir == rowDir && rowMove == true){
+			//Check for wall then move
+			isNextFree = checkNextSquare(localTraveler, currentDir);
+			moveTraveler(localTraveler);
+		}
+		else{
+			//TRY TO CHANGE DIRECTION
+		}
+	}
+}
+
+bool checkNextSquare(struct TravelerToPass localTraveler, Direction currentDir){
+	int currentRow = localTraveler.travelersPassed[0][localTraveler.travelerIdx].segmentList[0].row;
+	int currentCol = localTraveler.travelersPassed[0][localTraveler.travelerIdx].segmentList[0].col;
+
+	if (currentDir == Direction::EAST){
+		if(grid[currentRow][currentCol + 1] == SquareType::FREE_SQUARE){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	else if(currentDir == Direction::WEST){
+		if(grid[currentRow][currentCol - 1] == SquareType::FREE_SQUARE){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	else if(currentDir == Direction::NORTH){
+		if(grid[currentRow + 1][currentCol] == SquareType::FREE_SQUARE){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	else{
+		if(grid[currentRow - 1][currentCol] == SquareType::FREE_SQUARE){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
+
 
 //Use struct
 //Get the traveler to goal so give it a direction
@@ -399,19 +494,19 @@ void pathFinding(struct TravelerToPass localTraveler){
 		if(currentCol > exitPos.col){
 			//Check to see if tile is open
 			if(grid[currentRow][currentCol + 1] == SquareType::FREE_SQUARE){
-				currentDir = Direction::EAST;
+				currentDir = Direction::WEST;
 			}
 			else{
-				currentDir = Direction::WEST;
+				currentDir = Direction::EAST;
 			}
 		}
 		else{
 			//Check to see if tile is open
 			if(grid[currentRow][currentCol - 1] == SquareType::FREE_SQUARE){
-				currentDir = Direction::WEST;
+				currentDir = Direction::EAST;
 			}
 			else{
-				currentDir = Direction::EAST;
+				currentDir = Direction::WEST;
 			}
 		}
 	}
